aaeffe34b323ca6a29c6bbfff6862e77509ac79f
Nicolas Soel <86941298+Nicholas017@users.noreply.github.com>
2023-12-18 21:23:28 -0500

    Mi primer commit
    agregando los primeros archivos

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const forms = document.querySelector(".forms"),
+    pwShowHide = document.querySelectorAll(".eye-icon"),
+    links = document.querySelectorAll(".link");
+
+console.log(forms, pwShowHide, links);
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+
+    });
+});
